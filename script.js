b90550cfbb2b537a1dcaaf6e25e715a0446421e6
ProRite <ritesh.54757@gmail.com>
2025-01-03 13:13:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 gsap.from('.form-content', {
     opacity: 0,
-    y: 5,
-    duration:2,
-    scrub: true
+    x: 300,
+    duration: 4,
+    rotate: 360,
+    ease: "power3.out",
+});
+
+gsap.from('.head h1', {
+    opacity: 0,
+    x: 300,
+    duration: 4,
+    rotate: 360,
+    ease: "power3.out",
 });
